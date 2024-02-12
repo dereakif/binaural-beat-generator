d68a8c172c100ce3451cc243dcd43c911f35d367
Akif Dere <dereakif@gmail.com>
2024-02-12 16:25:11 +0300

    done: tailwind and theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{svelte,js,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {}
+  },
+  daisyui: {
+    themes: ["light", "dark", "forest","dim",{
+      solarized: {
+        "primary": "#2aa198",
+        "secondary": "#268bd2",
+        "accent": "#d33682",
+        "neutral": "#93a1a1",
+        "base-100": "#002b36",
+        "info": "#6c71c4",
+        "success": "#859900",
+        "warning": "#cb4b16",
+        "error": "#dc322f",
+      },
+    }],
   },
   plugins: [require('daisyui')]
 };
