35d2fc3ecf74fe059f08f83912560413ceddc385
Akif Dere <akif@ingosa.ai>
2024-04-04 13:03:16 +0300

    Adjust canvas size according to dpi and update stop button

--- src/components/FrequencyList.svelte
+++ src/components/FrequencyList.svelte
@@ -11,12 +11,11 @@
   let container;
   let canvas;
   let canvasContext;
+  let dpi;
 
-  // let amplitude = 250; // Height of the wave
-  // let maxAmplitude = 500; // Height of the wave
+  const amplitudeMargin = 40;
   let amplitude;
   let maxAmplitude;
-  let frequency = 32; // 1 Hz
   let phase = 0;
   let animationFrameId;
 
@@ -35,6 +34,8 @@
   let currentHz = null;
   let isPlaying = false;
   let isMuted = false;
+  let showPulse = localStorage.getItem('showPulse') === 'true' || true;
+  let showMarkers = localStorage.getItem('showMarkers') === 'true' || true;
 
   let binauralFrequencies = [
     { hz: 1, state: 'Delta', description: 'Lethargic' },
@@ -53,8 +54,8 @@
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 
     canvasContext.beginPath();
-    canvasContext.moveTo(0, canvas.height / 4);
-    canvasContext.lineTo(canvas.width, canvas.height / 4);
+    canvasContext.moveTo(0, canvas.height / (2 * dpi));
+    canvasContext.lineTo(canvas.width, canvas.height / (2 * dpi));
     canvasContext.strokeStyle = theme.primary;
     canvasContext.lineWidth = 4;
     canvasContext.stroke();
@@ -74,20 +75,22 @@
 
   $: amplitude = volume * maxAmplitude;
 
-  $: frequency = currentHz;
+  $: {
+    localStorage.setItem('showPulse', `${showPulse}`);
+    localStorage.setItem('showMarkers', `${showMarkers}`);
+  }
 
   onMount(() => {
-    amplitude = container.offsetHeight / 2;
-    maxAmplitude = container.offsetHeight;
-
     canvasContext = canvas.getContext('2d');
 
-    const dpi = window.devicePixelRatio || 1;
+    dpi = window.devicePixelRatio || 1;
     canvas.style.width = container.offsetWidth + 'px';
     canvas.style.height = container.offsetHeight + 'px';
     canvas.width = container.offsetWidth * dpi;
     canvas.height = container.offsetHeight * dpi;
     canvasContext.scale(dpi, dpi);
+    maxAmplitude = (container.offsetHeight * dpi - amplitudeMargin) / (2 * dpi);
+    amplitude = (maxAmplitude * dpi) / (2 * dpi);
 
     drawLine();
   });
@@ -99,15 +102,15 @@
     // Update the phase to animate the wave
     // time is provided /by requestAnimationFrame and is in milliseconds
     // Convert time from ms to seconds, and multiply by 2π for a full cycle
-    phase = (time / 1000) * 2 * Math.PI * frequency;
+    phase = (time / 1000) * 2 * Math.PI * currentHz;
 
     // Draw the sine wave
     canvasContext.beginPath();
     for (let x = 0; x < canvas.width; x++) {
       let y =
-        canvas.height / 4 +
+        canvas.height / (2 * dpi) +
         amplitude *
-          Math.sin((2 * Math.PI * frequency * x) / canvas.width - phase);
+          Math.sin((2 * Math.PI * currentHz * x) / canvas.width - phase);
       canvasContext[x === 0 ? 'moveTo' : 'lineTo'](x, y);
     }
     canvasContext.strokeStyle = theme.primary;
@@ -177,12 +180,14 @@
   };
 
   const adjustStep = () => {
-    const closestStep = frequencySteps.reduce((prev, curr) =>
-      Math.abs(curr - baseFrequency) < Math.abs(prev - baseFrequency)
-        ? curr
-        : prev
-    );
-    baseFrequency = closestStep;
+    if (showMarkers) {
+      const closestStep = frequencySteps.reduce((prev, curr) =>
+        Math.abs(curr - baseFrequency) < Math.abs(prev - baseFrequency)
+          ? curr
+          : prev
+      );
+      baseFrequency = closestStep;
+    }
     if (output.left && output.right) {
       output.left.frequency.value = baseFrequency;
       output.right.frequency.value = baseFrequency + currentHz;
@@ -198,108 +203,168 @@
   };
 </script>
 
-<div bind:this={container}>
+<div class={`mt-10 ${!showPulse ? 'hidden' : ''}`} bind:this={container}>
   <canvas bind:this={canvas} />
 </div>
 
-<div class="flex flex-wrap justify-center md:justify-between mb-10">
-  <div class="flex flex-col gap-6">
-    <div class="card w-96 h-fit bg-neutral text-neutral-content">
-      <div class="card-body">
-        <h2 class="card-title flex justify-between">
-          <div class="flex gap-1 items-end">
-            Base Frequency
-            <div
-              class="tooltip tooltip-info cursor-pointer"
-              data-tip="Foundational tone or frequency that is chosen for one ear, upon which the second tone (for the other ear) is based to create the desired binaural beat effect. "
+<div
+  class="mt-10 flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-6"
+>
+  <div class="card w-full max-w-96 h-fit bg-neutral text-neutral-content">
+    <div class="card-body">
+      <h2 class="card-title flex justify-between">
+        <div class="flex gap-1 items-end">
+          Base Frequency
+          <div
+            class="tooltip tooltip-info cursor-pointer"
+            data-tip="Foundational tone or frequency that is chosen for one ear, upon which the second tone (for the other ear) is based to create the desired binaural beat effect. "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="stroke-current shrink-0 w-6 h-6"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path></svg
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="stroke-current shrink-0 w-6 h-6"
-                ><path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path></svg
-              >
-            </div>
           </div>
-          <span class="font-mono text-base self-end text-secondary">
-            {baseFrequency}Hz
-          </span>
-        </h2>
-        <input
-          type="range"
-          class="range range-accent range-sm w-full"
-          min="32"
-          max="512"
-          bind:value={baseFrequency}
-          on:change={adjustStep}
-        />
-        <div class="w-full flex text-xs px-2">
-          <span>|</span>
-          <span class="ml-[6%]">|</span>
-          <span class="ml-[12%]">|</span>
-          <span class="ml-[25.4%]">|</span>
-          <span class="ml-[51.6%]">|</span>
         </div>
-      </div>
-    </div>
-    <div class="card w-96 h-fit bg-neutral text-neutral-content">
-      <div class="card-body">
-        <h2 class="card-title flex justify-between">
-          Volume
-          <label class="swap">
-            <input type="checkbox" checked={!isMuted} />
-            <svg
-              class="swap-on fill-secondary"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              ><path
-                d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
-              /></svg
-            >
-            <svg
-              class="swap-off fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              ><path
-                d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"
-              /></svg
-            >
-          </label>
-        </h2>
-        <input
-          type="range"
-          class="range range-accent w-80"
-          bind:value={volume}
-          min={0.0}
-          max={1.0}
-          step={0.01}
-        />
+        <span class="font-mono text-base self-end text-secondary">
+          {baseFrequency}Hz
+        </span>
+      </h2>
+      <input
+        type="range"
+        class="range range-accent range-sm"
+        min="32"
+        max="512"
+        bind:value={baseFrequency}
+        on:input={adjustStep}
+      />
+      <div class="w-full flex text-xs px-2 {!showMarkers ? 'invisible' : ''}">
+        <span>|</span>
+        <span class="ml-[6%]">|</span>
+        <span class="ml-[12%]">|</span>
+        <span class="ml-[25.4%]">|</span>
+        <span class="ml-[51.6%]">|</span>
       </div>
     </div>
   </div>
-  <div class="join join-vertical mt-10 md:mt-0">
-    {#each binauralFrequencies as { hz, state, description }}
-      <button
-        class="btn btn-primary btn-outline btn-sm join-item"
-        class:btn-active={currentHz === hz && isPlaying}
-        on:click={() => playBeat(hz)}
-      >
-        {hz} Hz - {state} - {description}
+  <div class="card w-full max-w-96 h-36 bg-neutral text-neutral-content">
+    <div class="card-body flex flex-row items-center justify-between">
+      <button on:click={stopBeat} class="btn btn-circle hover:scale-105">
+        <svg
+          class="fill-secondary cursor-pointer hover:fill-secondary"
+          height="48"
+          width="48"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          xml:space="preserve"
+        >
+          <path
+            d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M336,320
+  	c0,8.837-7.163,16-16,16H192c-8.837,0-16-7.163-16-16V192c0-8.837,7.163-16,16-16h128c8.837,0,16,7.163,16,16V320z"
+          />
+        </svg>
       </button>
-    {/each}
-    <button
-      class="btn btn-error btn-outline btn-sm join-item"
-      on:click={stopBeat}>Stop</button
-    >
+      <div class="form-control flex flex-col">
+        <label class="cursor-pointer label gap-2">
+          <span class="label-text">Show pulse</span>
+          <input
+            type="checkbox"
+            bind:checked={showPulse}
+            class="toggle toggle-xs toggle-secondary"
+          />
+        </label>
+        <label class="cursor-pointer label gap-2">
+          <span class="label-text">Show markers</span>
+          <input
+            type="checkbox"
+            bind:checked={showMarkers}
+            class="toggle toggle-xs toggle-secondary"
+          />
+        </label>
+      </div>
+    </div>
   </div>
+  <div class="card w-full max-w-96 h-fit bg-neutral text-neutral-content">
+    <div class="card-body">
+      <h2 class="card-title flex justify-between">
+        <div class="flex gap-1 items-end">
+          Volume
+          <div
+            class="tooltip tooltip-info cursor-pointer"
+            data-tip="You can adjust amplitude of the sound wave by changing the volume."
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="stroke-current shrink-0 w-6 h-6"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path></svg
+            >
+          </div>
+        </div>
+        <label class="swap">
+          <input type="checkbox" checked={!isMuted} />
+          <svg
+            class="swap-on fill-secondary"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            ><path
+              d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
+            /></svg
+          >
+          <svg
+            class="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            ><path
+              d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"
+            /></svg
+          >
+        </label>
+      </h2>
+      <input
+        type="range"
+        class="range range-accent range-sm mb-6"
+        bind:value={volume}
+        min={0.0}
+        max={1.0}
+        step={0.01}
+      />
+    </div>
+  </div>
+</div>
+<div
+  class="my-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 grid-flow-row gap-4"
+>
+  {#each binauralFrequencies as { hz, state, description }}
+    <button
+      class="btn btn-primary btn-outline join-item"
+      class:btn-active={currentHz === hz && isPlaying}
+      on:click={() => playBeat(hz)}
+    >
+      <div class="flex flex-col px-4 gap-1">
+        <span>
+          {hz} Hz - {state}
+        </span>
+        <span>{description}</span>
+      </div>
+    </button>
+  {/each}
 </div>
